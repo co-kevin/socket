16374e1c2bc3f1e6c5f9dba4cff1197b8542b808
Hooks Zhang <hookszhang@gmail.com>
2019-11-13 15:34:12 +0800

    feat: always read/send messages

--- client.c
+++ client.c
@@ -1,20 +1,19 @@
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#define BUFFER_SIZE 4096
 #define PORT 8080
 
 int main(int argc, const char *argv[]) {
     int sock = 0;
-    long valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1024] = {0};
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
+        printf("Socket creation error \n");
+        exit(EXIT_FAILURE);
     }
 
     serv_addr.sin_family = AF_INET;
@@ -22,18 +21,31 @@ int main(int argc, const char *argv[]) {
 
     // Convert IPv4 and IPv6 addresses from text to binary form
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\n Invalid address/ Address not supported \n");
-        return -1;
+        printf("Invalid address/ Address not supported \n");
+        exit(EXIT_FAILURE);
     }
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\n Connection Failed \n");
-        return -1;
+        printf("Connection Failed \n");
+        exit(EXIT_FAILURE);
     }
+    printf("Server connected\n");
 
-    send(sock, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    valread = read(sock, buffer, 1024);
-    printf("%s\n", buffer);
+    char buffer[BUFFER_SIZE];
+    long byteSend;
+    do {
+        fgets(buffer, BUFFER_SIZE, stdin);
+        if (buffer[0] == '\n') {
+            printf("Close the connection\n");
+            break;
+        }
+
+        byteSend = send(sock, buffer, strlen(buffer), 0);
+        if (byteSend != strlen(buffer)) {
+            printf("Send error\n");
+            exit(EXIT_FAILURE);
+        }
+    } while (1);
+    close(sock);
     return 0;
 }
--- server.c
+++ server.c
@@ -4,27 +4,19 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#define BUFFER_SIZE 4096
+#define QUEUE_SIZE 5
 #define PORT 8080
 
 int main(int argc, const char *argv[]) {
-    int server_fd, new_socket;
-    long valread;
+    int server_socket, client_socket;
     struct sockaddr_in address;
-    int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
 
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Forcefully attaching socket to the port
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEADDR, &opt,
-                   sizeof(opt))) {
-        perror("setsocketopt");
         exit(EXIT_FAILURE);
     }
 
@@ -33,25 +25,39 @@ int main(int argc, const char *argv[]) {
     address.sin_port = htons(PORT);
 
     // Forcefully attaching socket to the port
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    if (bind(server_socket, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, 3) < 0) {
+    printf("Server listening 0.0.0.0:%d \n", PORT);
+    if (listen(server_socket, QUEUE_SIZE) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+    if ((client_socket = accept(server_socket, (struct sockaddr *)&address,
                              (socklen_t *)&addrlen)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
+    printf("Client connected \n");
 
-    valread = read(new_socket, buffer, 1024);
-    printf("%s \n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
+    long byteRead;
+    do {
+        char buffer[BUFFER_SIZE];
+        byteRead = read(client_socket, buffer, BUFFER_SIZE);
+
+        if (byteRead <= 0) {
+            printf("Connection has already been closed \n");
+            break;
+        }
+
+        buffer[byteRead] = '\0';
+        printf("Message: %s", buffer);
+    } while (1);
+
+    close(server_socket);
+    close(client_socket);
     return 0;
 }
